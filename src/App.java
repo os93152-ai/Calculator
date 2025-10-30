89d9027fe3f50231df3b4be8c771ae15770d889a
os93152-ai <os93152@gmail.com>
2025-10-30 13:48:17 +0530

    Prabhjot | RED -- Single Number tests

--- src/App.java
+++ src/App.java
@@ -8,6 +8,12 @@ public class App {
         assertEquals("Test Case 1 : Failed", add(""), 0);
         assertEquals("Test Case 2 : Failed", add("0"), 0);
         assertEquals("Test Case 3 : Failed", add("1"), 1);
+        assertEquals("Test Case 3 : Failed", add("5"), 5);
+        assertEquals("Test Case 3 : Failed", add("10"), 10);
+        assertEquals("Test Case 3 : Failed", add("25"), 25);
+        assertEquals("Test Case 3 : Failed", add("153"), 153);
+        assertEquals("Test Case 3 : Failed", add("9368"), 9368);
+        assertEquals("Test Case 3 : Failed", add("9876543210"), 9876543210);
     }
 
     public static int add(String numbers){
