aea8a7c7ddbcff84717a41f8197c5812eba401dc
os93152-ai <os93152@gmail.com>
2025-10-30 14:50:21 +0530

    Prabhjot | RED -- Two number(comma separated) addition test

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.*; // For JUnit 5
+
 public class App {
 
     @Test
@@ -8,13 +11,14 @@ public class App {
         assertEquals("Test Case 1 : Failed", add(""), 0);
         assertEquals("Test Case 2 : Failed", add("0"), 0);
         assertEquals("Test Case 3 : Failed", add("1"), 1);
-        assertEquals("Test Case 3 : Failed", add("5"), 5);
-        assertEquals("Test Case 3 : Failed", add("10"), 10);
-        assertEquals("Test Case 3 : Failed", add("25"), 25);
-        assertEquals("Test Case 3 : Failed", add("153"), 153);
-        assertEquals("Test Case 3 : Failed", add("9368"), 9368);
-        assertEquals("Test Case 3 : Failed", add("9876543210"), 9876543210L);
-        assertEquals("Test Case 3 : Failed", add("98767485955524"), 98767485955524L);
+        assertEquals("Test Case 4 : Failed", add("5"), 5);
+        assertEquals("Test Case 5 : Failed", add("10"), 10);
+        assertEquals("Test Case 6 : Failed", add("25"), 25);
+        assertEquals("Test Case 7 : Failed", add("153"), 153);
+        assertEquals("Test Case 8 : Failed", add("9368"), 9368);
+        assertEquals("Test Case 9 : Failed", add("9876543210"), 9876543210L);
+        assertEquals("Test Case 10 : Failed", add("98767485955524"), 98767485955524L);
+        assertEquals("Test Case 11 : Failed", add("1,2"), 3);
     }
 
     public static long add(String numbers){
